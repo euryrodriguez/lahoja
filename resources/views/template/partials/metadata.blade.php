ee93806aa36a4213dacd3e6e213815e1278fd74b
euryrodriguez <eury958@gmail.com>
2021-01-31 11:50:21 -0300

    -Meta tags para SEO agregadas. -Captura de la app agregada -Archivos app.css y app.js minificados.

--- resources/views/template/partials/metadata.blade.php
+++ resources/views/template/partials/metadata.blade.php
@@ -1,28 +1,23 @@
+<meta name="title" content="Hojas de presentación para trabajos universitarios UASD | UTESA | ITSC | O&M">
+<meta name="description" content="Imprime tus hojas de presentación para trabajos universitarios UASD | UTESA | ITSC | O&M">
 <meta charset="utf-8">
-<meta name="title" content="">
-<meta name="description" content=""/>
-<meta name="keywords" content="UASD, ITSC, UTESA, Hoja, Presentación">
-<meta name="author" content="Eury Dariel Peña Rodríguez"/>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
-<meta name="robots" content="index, follow"/>
-<!--SEO-->
-<link rel="canonical" href=""/>
-<meta property="og:title" content=""/>
-<meta property="og:type" content="website"/>
+<link rel="canonical" href="https://lahoja.herokuapp.com/"/>
 <meta property="og:locale" content="es_ES"/>
-<meta property="og:url" content=""/>
-<meta property="og:description" content=""/>
-<meta property="og:image" content=""/>
-<meta property="og:site_name" content="La Hoja"/>
-<meta property="article:publisher" content=""/>
-<meta name="twitter:card" content=""/>
-<meta name="twitter:title" content=""/>
-<meta name="twitter:description" content=""/>
-<meta name="twitter:card" content=""/>
-<meta name="twitter:image:src" content=""/>
-<meta name="description" content=""/>
-<meta name="keywords" content="">
-<meta name="author" content=""/>
+<meta property="og:title" content="Hojas de presentación para trabajos universitarios UASD | UTESA | ITSC | O&M"/>
+<meta property="og:description"
+      content="Hojas de presentación para trabajos universitarios UASD | UTESA | ITSC | O&M"/>
+<meta property="og:url" content="https://lahoja.herokuapp.com/"/>
+<meta property="og:site_name" content="Hojas de presentación para trabajos universitarios UASD | UTESA | ITSC | O&M"/>
+<meta property="og:image"
+      content="{{ asset('img/captura-app.png')  }}"/>
+<meta property="og:image:secure_url"
+      content="{{ asset('img/captura-app.png')  }}"/>
+<meta property="og:image:width" content="521"/>
+<meta property="og:image:height" content="296"/>
+<meta property="og:image:alt" content="Hojas de presentación para trabajos universitarios UASD | UTESA | ITSC | O&M"/>
+<!-- / Yoast SEO Premium plugin. -->
+
+<meta property='og:image' content='{{ asset('img/captura-app.png')  }}'/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <meta name="robots" content="index, follow"/>
 <meta name="csrf-token" content="{{ csrf_token() }}">
